86e840bc9ae187e23f52d23018a645859375e45c
kerry <zihao317@gmail.com>
2016-06-14 09:15:11 +0800

    MVC 與 WebFrom 結合

---
+++ CrystalReportMVC/Controllers/ProductController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using CrystalReportMVC.Models;
+using CrystalDecisions.CrystalReports.Engine;
+
+using System.IO;
+
+namespace CrystalReportMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            // 資料庫類別
+            ProductRepository db = new ProductRepository();
+            //List<ProductModel> items =  db.GetAll();
+
+            // 取得資料
+            ProductDataSet ds = db.ExcuteDataSet();
+
+            // 報表文件類別
+            ReportClass reportdoc = new ReportClass();
+
+            // 讀取報表檔
+            reportdoc.FileName = Server.MapPath("~/Views/Product/ProductReport.rpt");
+
+            // 設定資料來源1
+            reportdoc.SetDataSource(ds);
+
+            // 設定輸出楁式為PDF
+            Stream stream = reportdoc.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+
+            return File(stream, "application/pdf");
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CrystalReportMVC/Models/ProductModel.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace CrystalReportMVC.Models
+{
+    /// <summary>
+    /// 產品
+    /// </summary>
+    public class ProductModel
+    {
+        public System.Guid Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public System.DateTime EffectivedDate { get; set; }
+    }
+
+    public class ProductRepository
+    {
+        /// <summary>
+        /// 使用物件
+        /// </summary>
+        /// <returns></returns>
+        public List<ProductModel> GetAll()
+        {
+            List<ProductModel> items = new List<ProductModel>();
+
+            try
+            {
+                // 連線字串
+                string connectionString = WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                // SQL
+                string sql = "select * from Product order by ProductName";
+                // 準備連線
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    // 取得資料方式 
+                    using (SqlCommand command = new SqlCommand(sql, conn))
+                    {
+                        // 連線取得方式是字串
+                        command.CommandType = System.Data.CommandType.Text;
+
+                        // 開起連線
+                        if (conn.State != System.Data.ConnectionState.Open)
+                            conn.Open();
+
+                        // 讀取資料方法
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // 開始讀取
+                            while (reader.Read())
+                            {
+                                // 將資料放至物件裡
+                                ProductModel item = new ProductModel
+                                {
+                                    Id = Guid.Parse(reader["Id"].ToString()),
+                                    ProductName = reader["ProductName"].ToString(),
+                                    Price = Convert.ToDecimal(reader["Price"].ToString()),
+                                    EffectivedDate = Convert.ToDateTime(reader["EffectivedDate"].ToString())
+                                };
+
+                                // 加入至物料集
+                                items.Add(item);
+                            }
+                        }
+
+
+                    }
+                }
+            }
+            catch { }
+            
+            return items;
+        }
+
+        /// <summary>
+        /// 使用資料集
+        /// </summary>
+        /// <returns>資料集</returns>
+        public ProductDataSet ExcuteDataSet()
+        {
+            ProductDataSet ds = new ProductDataSet();
+            DataTable dt = ds.Tables["Product"];
+
+            try
+            {
+                // 連線字串
+                string connectionString = WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                // SQL
+                string sql = "select * from Product order by ProductName";
+                // 準備連線
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    // 取得資料方式 
+                    using (SqlCommand command = new SqlCommand(sql, conn))
+                    {
+                        // 連線取得方式是字串
+                        command.CommandType = System.Data.CommandType.Text;
+
+                        // 開起連線
+                        if (conn.State != System.Data.ConnectionState.Open)
+                            conn.Open();
+
+                        // 讀取資料方法
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // 開始讀取
+                            while (reader.Read())
+                            {
+                                // 將資料放至物件裡
+                                DataRow dr = dt.NewRow();
+                                dr["Id"] = Guid.Parse(reader["Id"].ToString());
+                                dr["ProductName"] = reader["ProductName"].ToString();
+                                dr["Price"] = Convert.ToDecimal(reader["Price"].ToString());
+                                dr["EffectivedDate"] = Convert.ToDateTime(reader["EffectivedDate"].ToString());
+
+                                // 加入至物料集
+                                dt.Rows.Add(dr);
+                            }
+                        }
+
+
+                    }
+                }
+            }
+            catch { }
+
+            return ds;
+        }
+    }
+
+
+}
